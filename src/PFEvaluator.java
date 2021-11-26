c4716d885d39a7a42352cf9b840b14fecc007ce8
Pedro Lemos <pedrotlemos@gmail.com>
2021-11-26 15:12:37 -0300

    Atualizado para utilizar a classe de Tokens

--- src/PFEvaluator.java
+++ src/PFEvaluator.java
@@ -1,52 +1,78 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Scanner;
-import java.util.Stack;
+import java.util.*;
 
 public class PFEvaluator {
-    public static void main(String args[]) throws FileNotFoundException {
+    public static void main(String[] args) throws FileNotFoundException {
         File f = new File("Calc1.stk");
         Scanner in = new Scanner(f);
-        Stack<Integer> stack = new Stack<>();
-        boolean failed = false;
+        ArrayList<Token> tokenList = new ArrayList<Token>();
+        String[] validOp = new String[]{"+", "-", "*", "/"};
+        String invalidChar = "";
         while (in.hasNext()){
             String line = in.next();
             if (line.matches("\\d+")){
-                int number = Integer.parseInt(line);
-                stack.push(number);
+                Token token = new Token(TokenType.NUM, line);
+                tokenList.add(token);
+            }
+            else if (Arrays.asList(validOp).contains(line)){
+                Token token = null;
+                if (line.equals("+")){
+                    token = new Token(TokenType.PLUS, line);
+                }
+                else if (line.equals("-")){
+                    token = new Token(TokenType.MINUS, line);
+                }
+                else if (line.equals("/")){
+                    token = new Token(TokenType.SLASH, line);
+                }
+                else if (line.equals("*")){
+                    token = new Token(TokenType.STAR, line);
+                }
+                tokenList.add(token);
+            }
+            else{
+                Token token = new Token(TokenType.EOF, line);
+                tokenList.add(token);
+            }
+        }
+        Stack<Integer> opStack = new Stack<>();
+        boolean failed = false;
+        for (int i = 0; i < tokenList.size(); i++){
+            Token token = tokenList.get(i);
+            if (token.type.equals(TokenType.NUM)){
+                opStack.push(Integer.parseInt(token.lexeme));
+            }
+            else if (token.type.equals(TokenType.EOF)){
+                System.out.println("Unexpected character: " + token.lexeme + " on line " + (i + 1));
+                failed = true;
+                break;
             }
             else{
                 Integer first, second;
-                second = stack.pop();
-                first = stack.pop();
-                int result;
-                if (line.equals("+")){
-                    result = first + second;
+                second = opStack.pop();
+                first = opStack.pop();
+                if (token.type.equals(TokenType.PLUS)){
+                    opStack.push(first + second);
                 }
-                else if (line.equals("-")){
-                    result = first - second;
+                else if (token.type.equals(TokenType.MINUS)){
+                    opStack.push(first - second);
                 }
-                else if (line.equals("*")){
-                    result = first * second;
+                else if (token.type.equals(TokenType.SLASH)){
+                    opStack.push(first / second);
                 }
-                else if (line.equals("/")){
-                    result = first / second;
+                else if (token.type.equals(TokenType.STAR)){
+                    opStack.push(first * second);
                 }
-                else{
-                    System.out.println("Operação inválida!");
-                    failed = true;
-                    break;
-                }
-
-                stack.push(result);
             }
         }
         if (!failed){
-            System.out.println("Resultado: " + stack.pop());
+            for (Token token : tokenList) {
+                System.out.println("Token [type=" + token.type + ", lexeme=" + token.lexeme + "]");
+            }
+            System.out.println(opStack.pop());
         }
-        else{
-            System.out.println("Arquivo ilegível, por favor reescreva-o");
-        }
-
     }
 }
+
+
